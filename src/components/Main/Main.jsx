753341ecc951037d749413c38c36ec6684d369a4
excel <elim0079@student.monash.edu>
2025-11-14 16:59:32 +0800

    add in promt generation feature

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,78 +1,138 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Main.css';
-import { Code, CompassIcon, CookieIcon, ImagesIcon, Mic, Send, Trees, User } from 'lucide-react';
+import { AtomIcon, Code, CompassIcon, CookieIcon, ImagesIcon, Mic, Send, Trees, User, UserIcon } from 'lucide-react';
+import { Context } from '../../context/Context';
 
 const Main = () => {
-  return (
-    <div className='main'>
-        
-        <div className="nav">
-            <p>
-                Gemini 
-            </p>
-            <User className='user-icon'/>
-        </div>
 
-        <div className='main-container'>
-            <div className="greet">
+    const {
+        onSent, 
+        recentPrompt, 
+        showResult, 
+        loading, 
+        resultData, 
+        setInput, 
+        input
+    } = useContext(Context);
+
+    return (
+        <div className='main'>
+            
+            <div className="nav">
                 <p>
-                    <span>
-                        Hello, Ex.
-                    </span>
+                    Gemini 
                 </p>
-                <p>
-                    How can I help you today?
-                </p>
+                <User className='user-icon'/>
             </div>
 
-            <div className="cards">
-                <div className="card">
-                    <p>
-                        Suggest so Kuala Lumpur weekend getaways.
-                    </p>
-                    <CompassIcon className='card-icon'/>
-                </div>
+            <div className='main-container'>
 
-                <div className="card">
-                    <p>
-                        Summarise what is urban planning.
-                    </p>
-                    <Trees className='card-icon'/>
-                </div>
+                {!showResult ?
+                <>
+                    <div className="greet">
+                        <p>
+                            <span>
+                                Hello, Ex.
+                            </span>
+                        </p>
+                        <p>
+                            How can I help you today?
+                        </p>
+                    </div>
 
-                <div className="card">
-                    <p>
-                        Improve the readbility of the following code.
-                    </p>
-                    <Code className='card-icon'/>
-                </div>
+                    <div className="cards">
+                        <div className="card">
+                            <p>
+                                Suggest so Kuala Lumpur weekend getaways.
+                            </p>
+                            <CompassIcon className='card-icon'/>
+                        </div>
 
-                <div className="card">
-                    <p>
-                        Give me a chocolate chip cookies recipe.
-                    </p>
-                    <CookieIcon className='card-icon'/>
-                </div>
-            </div>
+                        <div className="card">
+                            <p>
+                                Summarise what is urban planning.
+                            </p>
+                            <Trees className='card-icon'/>
+                        </div>
 
-            <div className="main-bottom">
-                <div className="search-box">
-                    <input type="text" placeholder='Enter a prompt here.'/>
-                    <div>
-                        <ImagesIcon className='search-box-icon'/>
-                        <Mic className='search-box-icon'/>
-                        <Send className='search-box-icon'/>
+                        <div className="card">
+                            <p>
+                                Improve the readbility of the following code.
+                            </p>
+                            <Code className='card-icon'/>
+                        </div>
+
+                        <div className="card">
+                            <p>
+                                Give me a chocolate chip cookies recipe.
+                            </p>
+                            <CookieIcon className='card-icon'/>
+                        </div>
+                    </div>
+                </>
+                :
+                <div
+                    className='result'
+                >
+                    <div className='result-title'>
+                        <UserIcon 
+                            className='user-icon'
+                        />
+                        <p>
+                            {recentPrompt}
+                        </p>
+                    </div>
+
+                    <div className='result-data'>
+                        {/* fake Gemini Icon */}
+                        <AtomIcon 
+                            className='gemini-icon'
+                        /> 
+                        {loading ? 
+                        <div className='loader'>
+                            <hr />
+                            <hr />
+                            <hr />
+                        </div>
+                        :
+                        <p dangerouslySetInnerHTML={{__html:resultData}}>
+                        </p>
+                        }
                     </div>
                 </div>
+                }
+                
 
-                <p className='bottom-info'>
-                    Gemini may display inaccurate info, including about people, 
-                    so double-check its responses.
-                </p>
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input 
+                            onChange={(e) => setInput(e.target.value)}
+                            value={input}
+                            type="text" 
+                            placeholder='Enter a prompt here.'
+                        />
+                        <div>
+                            <ImagesIcon className='search-box-icon'/>
+                            <Mic className='search-box-icon'/>
+                            {input?
+                            <Send 
+                                onClick={() => onSent()} 
+                                className='search-box-icon'
+                            />
+                            :
+                            null
+                            }
+                        </div>
+                    </div>
+
+                    <p className='bottom-info'>
+                        Gemini may display inaccurate info, including about people, 
+                        so double-check its responses.
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
-  )
+        )
 }
 
 export default Main
