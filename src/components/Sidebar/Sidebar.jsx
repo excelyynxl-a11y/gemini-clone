1a8001eaeb9712f64b4a1b9880443a39f234db9d
excel <elim0079@student.monash.edu>
2025-11-14 16:59:55 +0800

    add responsive styling

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,18 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Sidebar.css';
 import { FileQuestionIcon, HistoryIcon, MenuIcon, MessageCircle, Plus, Settings } from 'lucide-react';
+import { Context } from '../../context/Context';
 
 const Sidebar = () => {
 
     const [extended, setExtended] = useState(false);
+    const {onSent, prevPrompts, setRecentPrompt, newChat} = useContext(Context);
 
+    const loadPrompt = async (prompt) => {
+        setRecentPrompt(prompt);
+        await onSent(prompt);
+    }
+ 
     return (
         <div className='sidebar'>
             <div className="top">
@@ -13,7 +20,10 @@ const Sidebar = () => {
                     className='menu'
                     onClick={() => setExtended(prev => !prev)}
                 />
-                <div className="new-chat">
+                <div 
+                    className="new-chat"
+                    onClick={() => newChat()}
+                >
                     <Plus />
                     {extended ? 
                     <p>
@@ -29,12 +39,19 @@ const Sidebar = () => {
                     <p className="recent-title">
                         Recent 
                     </p>
-                    <div className="recent-entry">
-                        <MessageCircle />
-                        <p>
-                            What is React...
-                        </p>
-                    </div>
+                    {prevPrompts.map((item, index) => {
+                        return(
+                            <div 
+                                className="recent-entry"
+                                onClick={() => loadPrompt(item)}
+                            >
+                                <MessageCircle />
+                                <p>
+                                    {item.slice(0, 18)}...
+                                </p>
+                            </div>
+                        )
+                    })}
                 </div>
                 :
                 null
